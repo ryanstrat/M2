a7026b02a6e890dbd4101a5cee894d6b0861e020
Kanan <kananpatel@gatech.edu>
2016-01-31 23:59:07 -0500

    Edited antlabmain for p5

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("tlight6");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Kanan Patel");
+                  toPrint += p5.toString("kpatel342");
 
         System.out.println(toPrint);
 
